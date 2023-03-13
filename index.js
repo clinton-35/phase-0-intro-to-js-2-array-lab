293f04810201819885f6749d73b4274d1cb3f12c
clinton <clinton.wambua@student.moringaschool.com>
2023-03-13 14:40:05 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name = "Ralph"){
+    cats.push (name)
+}
+function destructivelyPrependCat(name = "Bob"){
+    cats.unshift (name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name= "Broom"){
+    const cats2 = cats.slice();
+    cats2.push(name)
+    return cats2
+}
+function prependCat(name="Arnold"){
+    const cats2 =cats.slice();
+    cats2.unshift(name)
+    return cats2
+}
+function removeLastCat(){
+    const cats2 =cats.slice();
+    cats2.pop()
+    return cats2
+}
+function removeFirstCat(){
+    const cats2 =cats.slice();
+    cats2.shift()
+    return cats2
+    
+}
